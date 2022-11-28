1bee1221b2188b62ea7bad6488c65aa1e200be7f
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-28 15:05:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import PageTitle from './components/PageTitle.vue';
+
+export default {
+  components: {
+    PageTitle
+  }
+}
+</script>
+
+<template>
+  <div>
+    <PageTitle/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+@import "./style/global.scss";
+
+</style>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'Il titolo della pagina'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 class="my-5">{{ title }}</h1>
+</template>
+
+<style scoped></style>
+
+
