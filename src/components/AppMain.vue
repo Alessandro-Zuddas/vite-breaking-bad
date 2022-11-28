ffa3414fb533b97f73e03196b560c91997f42152
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-28 17:20:30 +0100

    Aggiunge caricamento prima di aver fillato i dati

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,7 +21,7 @@ import CharacterCard from './CharacterCard.vue';
         .then((response) => {
             this.characters = response.data;
             console.log(this.characters);
-            loaded = true;
+            this.loaded = true;
         });
     }
     }
@@ -33,11 +33,18 @@ import CharacterCard from './CharacterCard.vue';
             <div class="ms-container-sm px-3 py-2 d-flex align-items-center">
                 <strong>Found {{ this.characters.length }} characters</strong>
             </div>
-            <div class="ms-container-cards d-flex justify-content-between px-3 mt-3">
+            <div class="ms-container-cards d-flex justify-content-between px-3 mt-3"
+                v-if="this.loaded === true">
 
                 <!-- Implementare Componente Card -->
                <CharacterCard v-for="character in this.characters"
                 :info="character" />
+
+            </div>
+            <div class="ms-container-cards text-center px-3 mt-3"
+                v-else>
+
+                <i class="loading-icon fas fa-circle-notch"></i>
 
             </div>
         </div>
@@ -53,6 +60,19 @@ import CharacterCard from './CharacterCard.vue';
 .ms-container-sm{
     color: white;
     background-color: #212529;
+}
+
+.loading-icon{
+    font-size: 3.75rem;
+    margin-top: 6.25rem;
+    margin-bottom: 6.25rem;
+    animation: loading 1s linear infinite;
+}
+
+@keyframes loading {
+    0% {
+        transform: rotate(360deg);
+    }
 }
 
 </style>
